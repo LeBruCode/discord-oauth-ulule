c678b6f4a0bef7468c356006c3fbaa81e82d013a
Brunbrun <bruno.sagnardon@gmail.com>
2026-02-27 16:27:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,6 +7,52 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Discord OAuth Ulule backend running");
+});
+
+app.get("/login", (req, res) => {
+  const redirect = encodeURIComponent(process.env.REDIRECT_URI);
+
+  res.redirect(
+    `https://discord.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${redirect}&response_type=code&scope=identify`
+  );
+});
+
+app.get("/callback", async (req, res) => {
+  try {
+    const code = req.query.code;
+
+    const tokenResponse = await axios.post(
+      "https://discord.com/api/oauth2/token",
+      new URLSearchParams({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        grant_type: "authorization_code",
+        code,
+        redirect_uri: process.env.REDIRECT_URI,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+
+    const userResponse = await axios.get(
+      "https://discord.com/api/users/@me",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    res.send(`Connecté en tant que ${userResponse.data.username}`);
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.send("Erreur OAuth");
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
